1a1d97da987e0facb7210aee85561a4db9a437da
buzihuwo <buzihuwo@qq.com>
2018-06-26 23:03:23 +0800

    20180626

---
+++ lessonthree/src/main/java/com/zhaolong/lessonthree/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zhaolong.lessonthree;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LessonthreeApplication.class);
+    }
+
+}
